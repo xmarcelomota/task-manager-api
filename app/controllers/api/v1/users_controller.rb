45d0dfa894c811f4480dcd258ab3af4aff67f15f
Marcelo Mota <xmarcelomota@gmail.com>
2019-06-24 11:45:18 -0300

    Adding destroy action to the users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,6 +26,13 @@ class Api::V1::UsersController < ApplicationController
       render json: {errors: user.errors}, status: 422
     end
   end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
